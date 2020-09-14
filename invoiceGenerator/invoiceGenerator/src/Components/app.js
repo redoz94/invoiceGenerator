5e88adca555e28e714672206473146c643e64cec
Azfar Bilal <azfarbilal@Azfars-MacBook-Pro.local>
2020-09-14 17:24:14 +0500

    newer

---
+++ invoiceGenerator/invoiceGenerator/src/Components/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello from app.js</h1>
+      </div>
+    );
+  }
+}
