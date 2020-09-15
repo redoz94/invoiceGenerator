2b2d8e5216af3b60a282f089a50bfb22881d7e61
Azfar Bilal <azfarbilal@Azfars-MacBook-Pro.local>
2020-09-15 16:41:43 +0500

    na

--- invoiceGenerator/invoiceGenerator/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./myStyles.css";
-import App from "./Components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
---
+++ invoiceGenerator/invoiceGenerator1/src/Components/LoadCreateInvoice.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+export default function LoadCreateInvoice(props) {
+  return (
+    <Card>
+      <Card.Header as="h1">Create Invoice Form</Card.Header>
+      <Card.Body>The html form displays here. . .</Card.Body>
+    </Card>
+  );
+}
